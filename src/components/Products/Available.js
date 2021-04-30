50be0cdce6685da9d0ac56de6e1bec2bce7a692c
CodeWorld3 <lthompson1206@gmail.com>
2021-04-30 16:23:13 -0400

    updated pictures

--- src/components/Products/Available.js
+++ src/components/Products/Available.js
@@ -5,27 +5,38 @@ import classes from './AvailableStrains.module.css';
 const DUMMY_STRAINS = [
     {
       id: 'm1',
-      name: 'OG',
-      description: 'SATIVA',
+      name: 'RUNTZ',
+      description: 'Hybrid',
       price: 22.99,
+      thumbnail: "https://i.imgur.com/zfFZzH3.png"
     },
     {
       id: 'm2',
-      name: 'HAZE',
-      description: 'SATIVA',
+      name: 'BLUE DREAM',
+      description: 'Sativa',
       price: 16.5,
+      thumbnail: "https://i.imgur.com/5BloSm5.png"
     },
     {
       id: 'm3',
-      name: 'KUSH',
-      description: 'INDICA',
+      name: 'GG4',
+      description: 'Hybrid',
       price: 12.99,
+      thumbnail: "https://i.imgur.com/yUga1X9.png"
     },
     {
       id: 'm4',
       name: 'GELATO',
       description: 'HYBRID',
       price: 18.99,
+      thumbnail: "https://i.imgur.com/pRtaXeL.png"
+    },
+    {
+      id: 'm5',
+      name: 'WEDDING CAKE',
+      description: 'SATIVA',
+      price: 18.99,
+      thumbnail: "https://i.imgur.com/jSn9UsA.png"
     },
   ];
 
@@ -37,6 +48,7 @@ const AvailableStrains = () => {
     name={strains.name} 
     description={strains.description} 
     price={strains.price}
+    thumbnail={strains.thumbnail}
     />
     ));
     
--- src/components/Products/ProductItem/ProductItem.js
+++ src/components/Products/ProductItem/ProductItem.js
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 
+import Thumbnail from "../../Thumbnail";
+import { Container, Row, Col } from "../../Grid";
 import ProductItemForm from './ProductItemForm';
 import classes from './ProductItem.module.css';
 import CartContext from '../../../store/cart-context';
@@ -11,24 +13,35 @@ const ProductItem = (props) => {
 
     const addToCartHandler = (amount) => {
 cartCtx.addItem({
+   
     id: props.id,
     name: props.name,
     amount: amount,
-    price: props.price
+    price: props.price,
+    thumbnail: props.thumbnail
 });
     };
 
 return (
     <li className={classes.strain}>
+        <Container>
+            <Row>
+            <Col size="xs-4 sm-2">
+            <Thumbnail src={props.thumbnail} />
+            </Col>
+            <Col size="xs-8 sm-9">
     <div><h3>{props.name}</h3>
     <div className={classes.description}>{props.description}</div>
     <div className={classes.price}>{price}</div>
     </div>
+    
     <div>
         <ProductItemForm id={props.id} onAddToCart={addToCartHandler} />
     </div>
     <hr></hr>
-  
+    </Col>
+    </Row>
+    </Container>
 </li>
 );
 };
